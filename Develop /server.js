4045b0f909a2cb22e0429e8c4eaa5a25abc8a943
Jefry Romero <romerojefry@icloud.com>
2020-11-07 12:34:29 -0800

    setting my port 3000

--- Develop /server.js
+++ Develop /server.js
@@ -2,3 +2,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const express = require("express");
+
+const PORT= process.env.PORT || 3000;
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
